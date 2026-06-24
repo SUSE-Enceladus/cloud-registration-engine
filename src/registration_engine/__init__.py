39aac2ea267a928054a0764b47aeb2163324c78c
Sean Marlow's opencode agent <sean.marlow@suse.com>
2026-06-24 22:52:33 +0000

    Cleanup Rancher mentions
    The registration engine will be Generic

--- src/registration_engine/__init__.py
+++ src/registration_engine/__init__.py
@@ -16,6 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Rancher PAYG: Registration Engine."""
+"""Cloud PAYG: Registration Engine."""
 
 __version__ = "0.1.0"
